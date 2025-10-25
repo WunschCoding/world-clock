bf78c164cd13d31ce548ba99e1bada0f4b582707
Michaela <66178768+WunschCoding@users.noreply.github.com>
2025-10-25 11:24:58 +0200

    integrated update-fuction for time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTZ = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTZ.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTZ.format(
+    "H:mm:ss [<small>]A[</small>]"
+  );
+
+  //Vienna
+  let viennaElement = document.querySelector("#vienna");
+  let viennaDateElement = viennaElement.querySelector(".date");
+  let viennaTimeElement = viennaElement.querySelector(".time");
+  let viennaTZ = moment().tz("Europe/Vienna");
+  viennaDateElement.innerHTML = viennaTZ.format("MMMM Do YYYY");
+  viennaTimeElement.innerHTML = viennaTZ.format("H:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
